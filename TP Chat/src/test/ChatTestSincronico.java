85be954c3b7bafb2f91341f4005f3e78e715bb8c
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-05-24 16:30:43 -0300

    Proyecto creado

---
+++ TP Chat/src/test/ChatTestSincronico.java
@@ -0,0 +1,11 @@
+package test;
+
+import edu.isistan.client.Client;
+
+public class ChatTestSincronico {
+
+    public static void main (String [] args) {
+        Client c = new Client();
+
+    }
+}
