46caedae224974fd0002b5a5c1ada696f765d873
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-05-24 16:40:31 -0300

    Cambios en client.Client

--- TP Chat/src/edu/isistan/client/Client.java
+++ TP Chat/src/edu/isistan/client/Client.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("localhost", 6663);
+            Socket s = new Socket(args[0], 6663);
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
             ChatGUI gui = MainWindows.launchOrGet(new Callback(dos));
             new Thread(()-> {
@@ -38,8 +38,9 @@ public class Client {
                                 break;
 
                             case (Protocol.PRIVATE_MSG):
-                                user = dis.readUTF();
-                                String text = dis.readUTF();
+                                String userTo = dis.readUTF();
+                                gui.chatWith(userTo);
+                                break;
                         }
                     }
                 } catch (IOException e) {
@@ -48,7 +49,7 @@ public class Client {
                 }
             }).start();
             dos.writeByte(Protocol.HANDSHAKE);
-            dos.writeUTF("localhost");
+            dos.writeUTF(args[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
