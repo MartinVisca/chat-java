1e8a2a386fc35495769a6352a9d700c2bffa0e35
Martin-Guerra <martinalejandroguerra12@gmail.com>
2020-06-01 16:58:58 -0300

    Se eliminaron los comentarios que habian quedado

--- TP Chat/src/edu/isistan/client/Client.java
+++ TP Chat/src/edu/isistan/client/Client.java
@@ -28,18 +28,8 @@ public class Client {
         }
     }
 
-    public Client() {
-        try {
-            s = new Socket(PUERTO, 6663);
-            dos = new DataOutputStream(s.getOutputStream());
-            gui = MainWindows.launchOrGet(new Callback(dos));
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     public static void main(String[] args) {
-        Client c = new Client();
-        c.setName(args[0]);
+        Client c = new Client(args[0]);
         c.ejecutar();
     }
 
@@ -89,50 +79,4 @@ public class Client {
             e.printStackTrace();
         }
     }
-    }
-        /*try {
-            Socket s = new Socket("localhost", 6663);
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            ChatGUI gui = MainWindows.launchOrGet(new Callback(dos));
-            new Thread(()-> {
-                try {
-                    DataInputStream dis = new DataInputStream(s.getInputStream());
-                    while (true) {
-                        byte type = dis.readByte();
-                        switch (type) {
-                            case (Protocol.ADD_USER):
-                                String user = dis.readUTF();
-                                gui.addUser(user);
-                                break;
-
-                            case (Protocol.REMOVE_USER):
-                                user = dis.readUTF();
-                                gui.removeUser(user);
-                                break;
-
-                            case (Protocol.GENERAL_MSG):
-                                user = dis.readUTF();
-                                String text = dis.readUTF();
-                                gui.addNewGeneralMsg(user, text);
-                                break;
-
-                            case (Protocol.PRIVATE_MSG):
-                                String userName = dis.readUTF();
-                                String userTo = dis.readUTF();
-                                String textTo = dis.readUTF();
-                                gui.addNewMsg(userName, textTo);
-                                break;
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(0);
-                }
-            }).start();
-            dos.writeByte(Protocol.HANDSHAKE);
-            dos.writeUTF(args[0]);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}*/
+}
